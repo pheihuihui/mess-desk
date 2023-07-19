029969e1d93122721ce4ea726532384e158c8e6c
Fei <33122873+pheihuihui@users.noreply.github.com>
2023-07-19 22:55:34 +0800

    ...

--- build.js
+++ build.js
@@ -11,7 +11,9 @@ fs.mkdirSync(dir_client, { recursive: true })
 fs.copyFileSync('./resources/favicon.ico', `${dir_client}/favicon.ico`)
 fs.copyFileSync('./resources/main.html', `${dir_client}/main.html`)
 fs.copyFileSync('./resources/client.css', `${dir_client}/client.css`)
-fs.copyFileSync('./resources/comrak.wasm', `${dir_client}/comrak.wasm`)
+
+const dir_wasm_pack = './wasm/comrak/pkg'
+fs.cpSync(dir_wasm_pack, `${dir_client}/pkg`, { recursive: true })
 
 es.buildSync({
     entryPoints: ['./src/index.ts'],
